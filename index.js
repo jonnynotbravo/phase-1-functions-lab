6a5f09c5bc173ca7490b7c75bb2de5c0beb44f48
Jonny Tilahun <Jonnybiruk13@gmail.com>
2022-10-11 13:07:46 -0400

    commit tree a2f0d40b83ccf5899fa38e2434c412088a9a7449

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+
+    return someValue >= 42 ? someValue - 42 : 42 - someValue;
+
+}
+
+function distanceFromHqInFeet(someValue){
+
+    return someValue >= 42 ? (someValue - 42) * 264 : (42 - someValue) * 264;
+
+}
+
+function distanceTravelledInFeet(start, finish){
+    return (start - finish) * 264 < 0 ? ((start - finish) * 264) * - 1 : (start - finish) * 264; 
+
+}
+
+function calculatesFarePrice(start, destination){
+
+    let traveFeet = distanceTravelledInFeet(start, destination);
+
+    let distance = traveFeet - 400;
+
+    return traveFeet > 400 && traveFeet < 2000 ? distance * 0.02 : traveFeet > 2000 && traveFeet < 2500 ? 25 : traveFeet < 400 ? 0 : 'cannot travel that far';
+    
+}
